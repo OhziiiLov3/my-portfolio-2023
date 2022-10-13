7330e218173880b23a15b78ec710267b7b5d9028
ohziiilov3 <klbaskerville0520@gmail.com>
2022-10-13 01:20:13 -0700

    added work component, end of the night push

---
+++ src/components/Work.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import WorkImg from '../assets/projects/Project1Img.png'
+import Project2Img from '../assets/projects/Project2Img.png'
+
+
+const Work = () => {
+  return (
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#457b9d]'>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-[#f4a261]'>Work</p>
+                <p className='py-6'>// Check out some of my recent work</p>
+            </div>
+
+            <div style={{backgroundImage: `url(${WorkImg})` }} 
+            className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+                <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                  
+                  
+                   {/* Hover Effects */}
+                    <div>
+                        <span>
+
+                        </span>
+                        <div>
+                            <a href="/">
+                                <button></button>
+                            </a>
+                            <a href="/">
+                                <button></button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Work
