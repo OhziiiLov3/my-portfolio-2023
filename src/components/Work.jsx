55117de1d175f729905780722b85e5ad07cf5549
ohziiilov3 <klbaskerville0520@gmail.com>
2022-10-13 11:40:13 -0700

    work component complete, added temp projects

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import WorkImg from '../assets/projects/Project1Img.png'
 import Project2Img from '../assets/projects/Project2Img.png'
+import Project3Img from '../assets/projects/Project3Img.png'
 
 
 const Work = () => {
@@ -11,16 +12,99 @@ const Work = () => {
                 <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-[#f4a261]'>Work</p>
                 <p className='py-6'>// Check out some of my recent work</p>
             </div>
-
-            <div style={{backgroundImage: `url(${WorkImg})` }} 
-            className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-                <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-                  
-                  
+            {/* Container */}
+            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+                {/* Grid Item */}
+                <div style={{backgroundImage: `url(${WorkImg})` }}  className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
                    {/* Hover Effects */}
                     <div className='opacity-0 group-hover:opacity-100'>
                         <span className='text-2xl font-bold text-white tracking-wider'>
                             React Js Application
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href="/">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                            </a>
+                            <a href="/">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                 {/* Grid Item */}
+                <div style={{backgroundImage: `url(${Project2Img})` }}  className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                   {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            Full Stack Python/Django Application
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href="/">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                            </a>
+                            <a href="/">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                 {/* Grid Item */}
+                <div style={{backgroundImage: `url(${Project3Img})` }}  className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                   {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            ReactJs-Axios Weather Application
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href="/">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                            </a>
+                            <a href="/">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                             {/* Grid Item */}
+                <div style={{backgroundImage: `url(${WorkImg})` }}  className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                   {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            React Js Application
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href="/">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                            </a>
+                            <a href="/">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                 {/* Grid Item */}
+                <div style={{backgroundImage: `url(${Project2Img})` }}  className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                   {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            Full Stack Python/Django Application
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href="/">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                            </a>
+                            <a href="/">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                 {/* Grid Item */}
+                <div style={{backgroundImage: `url(${Project3Img})` }}  className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                   {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            ReactJs-Axios Weather Application
                         </span>
                         <div className='pt-8 text-center'>
                             <a href="/">
